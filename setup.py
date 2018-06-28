a5bdacb7d3da6f021a3019227471de855d2da1d3
Christian Burkert <burkert@informatik.uni-hamburg.de>
2018-06-28 11:55:19 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='fuzzy-commitment',
+    version='0.1.0',
+    description='A fuzzy commitment scheme originally presented by Juels and Wattenberg',
+    long_description=readme(),
+    author='Christian Burkert',
+    url='https://github.com/cburkert/fuzzy-commitment',
+    packages=find_packages(),
+    install_requires=[
+        'bchlib',
+        'BitVector',
+    ],
+)
