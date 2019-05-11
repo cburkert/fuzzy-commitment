5cdea70354424bbd7b28654fe532abb940f91b13
Christian Burkert <burkert@informatik.uni-hamburg.de>
2019-05-11 19:30:24 +0200

    Clean up tests

--- fcs/test_fcs.py
+++ fcs/test_fcs.py
@@ -6,6 +6,14 @@ import secrets
 import fcs
 
 
+def random_flip(witness: bytes, numbits: int) -> bytes:
+    witness_mod = bytearray(witness)
+    bit_nums = random.sample(range(len(witness) * 8), numbits)
+    for bit_num in bit_nums:
+        witness_mod[bit_num // 8] ^= (1 << (bit_num % 8))
+    return bytes(witness_mod)
+
+
 class TestFCS(unittest.TestCase):
     def setUp(self):
         self.threshold = 2
@@ -13,24 +21,17 @@ class TestFCS(unittest.TestCase):
         self.cs = fcs.FCS(len(self.witness)*8, self.threshold)
         self.commitment = self.cs.commit(self.witness)
 
-    def random_flip_witness(self, numbits: int) -> bytes:
-        witness_mod = bytearray(self.witness)
-        bit_nums = random.sample(range(len(self.witness) * 8), numbits)
-        for bit_num in bit_nums:
-            witness_mod[bit_num // 8] ^= (1 << (bit_num % 8))
-        return bytes(witness_mod)
-
     def test_unaltered_witness(self):
         valid, _key = self.cs.verify(self.commitment, self.witness)
         self.assertTrue(valid)
 
     def test_altered_tolerable(self):
-        witness_mod = self.random_flip_witness(self.threshold)
+        witness_mod = random_flip(self.witness, self.threshold)
         valid, _key = self.cs.verify(self.commitment, witness_mod)
         self.assertTrue(valid)
 
     def test_altered_intolerable(self):
-        witness_mod = self.random_flip_witness(self.threshold + 1)
+        witness_mod = random_flip(self.witness, self.threshold + 1)
         valid, _key = self.cs.verify(self.commitment, witness_mod)
         self.assertFalse(valid)
 
@@ -43,24 +44,17 @@ class TestFCSTwo(unittest.TestCase):
         self.cs = fcs.FCS(len(self.witness)*8, self.threshold)
         self.commitment = self.cs.commit(self.witness)
 
-    def random_flip_witness(self, numbits: int) -> bytes:
-        witness_mod = bytearray(self.witness)
-        bit_nums = random.sample(range(len(self.witness) * 8), numbits)
-        for bit_num in bit_nums:
-            witness_mod[bit_num // 8] ^= (1 << (bit_num % 8))
-        return bytes(witness_mod)
-
     def test_unaltered_witness(self):
         valid, _key = self.cs.verify(self.commitment, self.witness)
         self.assertTrue(valid)
 
     def test_altered_tolerable(self):
-        witness_mod = self.random_flip_witness(self.threshold)
+        witness_mod = random_flip(self.witness, self.threshold)
         valid, _key = self.cs.verify(self.commitment, witness_mod)
         self.assertTrue(valid)
 
     def test_altered_intolerable(self):
-        witness_mod = self.random_flip_witness(self.threshold+1)
+        witness_mod = random_flip(self.witness, self.threshold+1)
         valid, _key = self.cs.verify(self.commitment, witness_mod)
         self.assertFalse(valid)
 
