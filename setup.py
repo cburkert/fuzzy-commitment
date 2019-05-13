867c573923a4235b5db50bfa06099d163983e8fd
Christian Burkert <burkert@informatik.uni-hamburg.de>
2019-05-13 14:18:02 +0200

    Prepare new release v1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name='fuzzy-commitment',
-    version='0.1.2',
+    version='1.0.0',
     description='A fuzzy commitment scheme originally presented by Juels and Wattenberg',
     long_description=readme(),
     author='Christian Burkert',
